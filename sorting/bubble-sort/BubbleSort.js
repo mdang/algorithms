1cc8ce8ae2b1a3a6d9c60f154239038c6ea7d8d9
Mike Dang <mike.dang@generalassemb.ly>
2019-04-24 21:13:06 -0500

    Formatting changes

--- sorting/bubble-sort/BubbleSort.js
+++ sorting/bubble-sort/BubbleSort.js
@@ -6,9 +6,9 @@ export default class BubbleSort {
     const array = [...originalArray] 
 
     // We'll need to make at least as many passes as there are elements in the array
-    for (let i=1; i<array.length; i++) {
+    for (let i = 1; i < array.length; i++) {
       // Each iteration do a swap whenever we come across two adjacent values in the wrong place
-      for (let j=0; j<array.length; j++) {
+      for (let j = 0; j < array.length; j++) {
         if (array[j] > array[j + 1]) {
           const tmp = array[j + 1]
           array[j + 1] = array[j]
