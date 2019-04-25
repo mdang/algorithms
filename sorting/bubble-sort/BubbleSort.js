96617fc5660a85ec197729e84dbfc09d9d3c3851
Mike Dang <mike.dang@generalassemb.ly>
2019-04-24 21:01:53 -0500

    Comment update

--- sorting/bubble-sort/BubbleSort.js
+++ sorting/bubble-sort/BubbleSort.js
@@ -2,12 +2,12 @@ export default class BubbleSort {
   sort(originalArray) {
     // Keep track of whether a swap took place or not 
     let swapped = false
-    // Clone original array
+    // Clone of original array for testing
     const array = [...originalArray] 
 
     // We'll need to make at least as many passes as there are elements in the array
     for (let i=1; i<array.length; i++) {
-      // Each iteration swap whenever we come across two adjacent numbers in the wrong place
+      // Each iteration do a swap whenever we come across two adjacent values in the wrong place
       for (let j=0; j<array.length; j++) {
         if (array[j] > array[j + 1]) {
           const tmp = array[j + 1]
