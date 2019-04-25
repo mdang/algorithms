d7638d9869762bc8806f18522fd9c8b31eadc1d8
Mike Dang <mike.dang@generalassemb.ly>
2019-04-24 23:03:53 -0500

    Insertion sort working and tested

--- sorting/insertion-sort/InsertionSort.js
+++ sorting/insertion-sort/InsertionSort.js
@@ -2,7 +2,23 @@ export default class InsertionSort {
   sort(originalArray) {
     const array = [...originalArray]
 
+    // Go through each element 
+    for (let i = 0; i < array.length; i++) {
+      let currentIndex = i
 
+      // Make sure we didn't reach the end
+      // Have to check for "undefined" explicitly because negative numbers are falsy and will prematurely end the sort
+      while (array[currentIndex - 1] !== undefined && 
+        (array[currentIndex] < array[currentIndex - 1])) {
+        // Keep swapping with each value that it's less than 
+        const tmp = array[currentIndex - 1]
+        array[currentIndex - 1] = array[currentIndex]
+        array[currentIndex] = tmp
+
+        // Shift the index to start from
+        currentIndex--
+      }
+    }
 
     return array
   }
