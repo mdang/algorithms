dae938a65938a22189fefcc1f4a67ac39ec7a24b
Mike Dang <mike.dang@generalassemb.ly>
2019-05-02 10:36:28 -0500

    Implemented binary search

--- search/binary-search/BinarySearch.js
+++ search/binary-search/BinarySearch.js
@@ -5,6 +5,10 @@ export default function binarySearch(sortedArray, item, start = 0, end = sortedA
   }
   
   // Determine the middle index
+  // "(start + end) / 2" works but doesn't take into account edge cases where 
+  // start and end integers are too large and can't be stored in a 32 bit system and cause 
+  // errors in program execution 
+  // "start + (end - start)/2"" will be safe for those integer values 
   const midpoint = Math.floor((start + end) / 2)
 
   // Is this the value we're searching for? 
